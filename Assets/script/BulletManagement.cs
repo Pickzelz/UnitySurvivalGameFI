6158f6dd00b7effa4809017197453c04c0229081
fandi <pickzelz@gmail.com>
2017-05-07 17:33:23 +0700

    - add character and multiplayer management

---
+++ Assets/script/BulletManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManagement : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("colltion masuk");
+        GameObject hit = collision.gameObject;
+        HealthManagement Health = hit.GetComponent<HealthManagement>();
+
+        if(Health != null)
+        {
+            Health.TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/script/HealthManagement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManagement : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public int currentHealth = 0;
+    public RectTransform healthBar;
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!!!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
